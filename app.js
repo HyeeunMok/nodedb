16deeb32361624ebc2d920befc847c8f0d3ad3fd
Hyeeun <hyeeuni.mok@gmail.com>
2019-12-25 11:32:46 -0500

    user by ID

--- app.js
+++ app.js
@@ -5,7 +5,33 @@ const server = app.listen(8080, function() {
   console.log('Server ready');
 })
 
+app.get('/users/:id', function(req, res) {
+  const query = "SELECT * from users where id = ?";
+  console.log(req.params.id);
+  const params = [req.params.id];
+  db.all(query,params, function (err, row) {
+    if (err) {
+      throw err;
+    }
+    res.json({
+      'status': row
+    })
+  })
+})
+
+app.get('/users', function (req, res) {
+  const query = "SELECT * from users";
+  db.all(query, function (err, rows) {
+    if (err) {
+      throw err;
+    }
+    res.json({
+      'status': rows
+    })
+  })
+})
+
 app.get('/',function(req, res) {
-res.json({'staus': 'ready'})
+  res.json({'staus': 'ready'})
 })
 
